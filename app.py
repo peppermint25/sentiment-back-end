01c6a5564d34e9660523160e502f0f6087649371
peppermint25 <oskars.metr01@gmail.com>
2024-01-16 23:30:36 +0200

    updates

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, session, redirect
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt
 from flask_cors import CORS, cross_origin
-from functools import wraps
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson.json_util import dumps
@@ -278,9 +277,9 @@ def get_completion(prompt, model="gpt-4-1106-preview"):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0,
     )
     return response.choices[0].message["content"]
+
 
 def convert_to_json(response):
     try:
